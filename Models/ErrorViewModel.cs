d917be583bce441e7afc1416a5afa9abe1317059
Dima <micleusanudumitru@.com>
2026-05-01 15:09:01 +0300

    Initial commit - DrDanielaVintu project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrDanielaVintu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
